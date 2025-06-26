2bd4554d4ad4a8f3dfc454abe8ff54a988ae6d10
John Siracusa <siracusa@gmail.com>
2025-06-26 10:38:25 -0400

    Initial Commit

---
+++ Worker/WorkerApp.swift
@@ -0,0 +1,17 @@
+//
+//  WorkerApp.swift
+//  Worker
+//
+//  Created by John Siracusa on 6/26/25.
+//
+
+import SwiftUI
+
+@main
+struct WorkerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
